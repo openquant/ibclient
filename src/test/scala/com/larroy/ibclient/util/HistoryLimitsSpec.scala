dbe08ab3e39acff720a6dae80a9d9b68a0da1dc2
Pedro Larroy <pedro.larroy@gmail.com>
2015-03-07 17:26:59 +0100

    Add HistoryLimits test

--- src/test/scala/com/larroy/ibclient/util/HistoryLimitsSpec.scala
+++ src/test/scala/com/larroy/ibclient/util/HistoryLimitsSpec.scala
@@ -4,7 +4,6 @@ package com.larroy.ibclient.util
  * @author piotr 03.03.15
  */
 
-import com.ib.client.Types.{DurationUnit, BarSize}
 import org.joda.time.format.DateTimeFormat
 import org.specs2.mutable._
 
@@ -13,19 +12,22 @@ class HistoryLimitsSpec extends Specification {
     "return propper HistoryDuration" in {
       val fmt = DateTimeFormat.forPattern("yyyyMMdd kk:mm:ss")
       def toDate(s: String) = fmt.parseDateTime(s).toDate
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._5_secs) shouldEqual new HistoryDuration(3600, 10000, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._30_secs) shouldEqual new HistoryDuration(3600, 10000, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._1_min) shouldEqual new HistoryDuration(3600, 86400, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._5_mins) shouldEqual new HistoryDuration(3600, 86400, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._15_mins) shouldEqual new HistoryDuration(3600, 86400, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._30_mins) shouldEqual new HistoryDuration(3600, 86400, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._1_hour) shouldEqual new HistoryDuration(3600, 86400, DurationUnit.SECOND)
-      HistoryLimits.bestDuration(toDate("20150303 10:00:00"), toDate("20150303 10:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(1,60, DurationUnit.DAY)
-      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(1,60, DurationUnit.DAY)
-      HistoryLimits.bestDuration(toDate("20150301 09:00:00"), toDate("20150303 09:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(2,60, DurationUnit.DAY)
-      HistoryLimits.bestDuration(toDate("20150203 09:00:00"), toDate("20150303 09:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(4, 52, DurationUnit.WEEK)
-      HistoryLimits.bestDuration(toDate("20150201 09:00:00"), toDate("20150303 09:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(1, 12, DurationUnit.MONTH)
-      HistoryLimits.bestDuration(toDate("20150131 09:00:00"), toDate("20150303 09:00:00"), BarSize._1_day) shouldEqual new HistoryDuration(2, 12, DurationUnit.MONTH)
+      import com.ib.client.Types.DurationUnit._
+      import com.ib.client.Types.BarSize._
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _5_secs) shouldEqual new HistoryDuration(3600, 10000, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _30_secs) shouldEqual new HistoryDuration(3600, 10000, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _1_min) shouldEqual new HistoryDuration(3600, 86400, SECOND)
+      HistoryLimits.bestDuration(toDate("20150101 09:00:00"), toDate("20150303 10:00:00"), _1_min) shouldEqual new  HistoryDuration(62,1,DAY)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _5_mins) shouldEqual new HistoryDuration(3600, 86400, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _15_mins) shouldEqual new HistoryDuration(3600, 86400, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _30_mins) shouldEqual new HistoryDuration(3600, 86400, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _1_hour) shouldEqual new HistoryDuration(3600, 86400, SECOND)
+      HistoryLimits.bestDuration(toDate("20150303 10:00:00"), toDate("20150303 10:00:00"), _1_day) shouldEqual new HistoryDuration(1,60, DAY)
+      HistoryLimits.bestDuration(toDate("20150303 09:00:00"), toDate("20150303 10:00:00"), _1_day) shouldEqual new HistoryDuration(1,60, DAY)
+      HistoryLimits.bestDuration(toDate("20150301 09:00:00"), toDate("20150303 09:00:00"), _1_day) shouldEqual new HistoryDuration(2,60, DAY)
+      HistoryLimits.bestDuration(toDate("20150203 09:00:00"), toDate("20150303 09:00:00"), _1_day) shouldEqual new HistoryDuration(4, 52, WEEK)
+      HistoryLimits.bestDuration(toDate("20150201 09:00:00"), toDate("20150303 09:00:00"), _1_day) shouldEqual new HistoryDuration(1, 12, MONTH)
+      HistoryLimits.bestDuration(toDate("20150131 09:00:00"), toDate("20150303 09:00:00"), _1_day) shouldEqual new HistoryDuration(2, 12, MONTH)
     }
   }
 }
