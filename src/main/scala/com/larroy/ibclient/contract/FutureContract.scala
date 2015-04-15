0e0618187f73190351595ada5d139c68f5f61d80
Pedro Larroy <pedro.larroy@here.com>
2015-04-15 14:46:16 +0200

    documentation

--- src/main/scala/com/larroy/ibclient/contract/FutureContract.scala
+++ src/main/scala/com/larroy/ibclient/contract/FutureContract.scala
@@ -3,8 +3,12 @@ package com.larroy.ibclient.contract
 import com.ib.client.Contract
 import com.ib.client.Types.SecType
 
+
 /**
- * @author piotr 10.02.15
+ * @param symbol
+ * @param expiry Expiry date in format yyyymmdd identifies the futures contract
+ * @param exchange for example NYMEX for commodity futures
+ * @param currency in which the future is denominated
  */
 class FutureContract(symbol: String, expiry: String, exchange: String = "GLOBEX", currency: String = "USD") extends Contract {
   symbol(symbol)
