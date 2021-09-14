d61324c208ce6f45fd18cab8c685cad751c88ba5
aryzach <zach_asmith@yahoo.com>
2021-09-14 14:04:45 -0500

    allow set account allocation on buy/sell orders

--- src/main/scala/com/larroy/ibclient/order/Buy.scala
+++ src/main/scala/com/larroy/ibclient/order/Buy.scala
@@ -7,4 +7,4 @@ import com.larroy.ibclient.order.kind.Kind
  * Buy Order
  * @author piotr 19.02.15
  */
-case class Buy(override val kind: Kind, override val quantity: Int) extends Order
+case class Buy(override val kind: Kind, override val quantity: Int, override val account: Option[String] = None) extends Order
